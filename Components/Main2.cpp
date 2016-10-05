8341372ede1625a0d39137859453ebfbff63b773
kelsey.davis <kelsey.davis@student.aie.edu.au>
2016-10-05 12:27:31 -0700

    hjds

---
+++ Components/Main2.cpp
@@ -0,0 +1,52 @@
+#include "sfwdraw.h"
+#include "vec2.h"
+#include "Transform.h"
+#include "fops.h"
+#include "rigidbody.h"
+#include "SpaceshipLocomotion.h"
+#include "SpaceshipController.h"
+
+void main()
+{
+	float SCREEN_WIDTH = 1200, SCREEN_HEIGHT = 1200;
+	sfw::initContext(SCREEN_WIDTH, SCREEN_HEIGHT);
+	float steps = 100;
+
+	vec2 start = { 200, 300 },
+		end = { 900, 800 },
+		mid = { 0, 1100 };
+
+	Transform playerTransform(200, 200);
+	playerTransform.scale = { 24,24 };
+
+	Rigidbody playerRigidbody;
+
+	SpaceshipController playerCtrl;
+	SpaceshipLocomotion playerLoco;
+
+	while (sfw::stepContext())
+	{
+		float deltaTime = sfw::getDeltaTime();
+
+		// Wrap the player's position within the screen bounds
+		if (playerTransform.position.x > SCREEN_WIDTH)
+			playerTransform.position.x = 0.0f;
+		else if (playerTransform.position.x < 0.0f)
+			playerTransform.position.x = SCREEN_WIDTH;
+
+		if (playerTransform.position.y > SCREEN_HEIGHT)
+			playerTransform.position.y = 0.0f;
+		else if (playerTransform.position.y < 0.0f)
+			playerTransform.position.y = SCREEN_HEIGHT;
+
+		// Apply rigidbody forces
+		playerCtrl.update(playerLoco);
+		playerLoco.update(playerTransform, playerRigidbody);
+		playerRigidbody.integrate(playerTransform, deltaTime);
+
+		// Draw the player
+		playerTransform.debugDraw();
+		playerRigidbody.debugDraw(playerTransform);
+	}
+	sfw::termContext();
+}
