c3268849693d09a3945b2cb044ab15cf346b8e7b
kelsey.davis <kelsey.davis@student.aie.edu.au>
2016-10-24 12:21:54 -0700

    stuff creayed

---
+++ MathLib/shapes.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Mat3.h"
+
+//Boundign Volumes
+//Base stete of a more complex code.
+struct Circle
+{
+public:
+	vec2 pos;
+	float rad;
+};
+
+
+struct AABB {};
+struct Plane {};
+struct Ray {};
+struct Hull {};
+
+Circle operator*(const mat3 &T, const Circle &C);
+AABB operator*(const mat3 &T, const AABB &B);
+Plane operator*(const mat3 &T, const Plane &P);
+Ray operator*(const mat3 &T, const Ray &R);
+Hull operator*(const mat3 &T, const Hull &H);
