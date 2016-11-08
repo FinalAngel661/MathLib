ef318bd5de7368a1d59be181ae2a599561d52008
kelsey.davis <kelsey.davis@student.aie.edu.au>
2016-11-08 10:08:24 -0800

    hull under development

--- MathLib/shapes.h
+++ MathLib/shapes.h
@@ -27,11 +27,21 @@ struct Plane
 };
 
 struct Ray {};
-struct Hull {};
+struct Hull 
+{
+	vec2 vertices[16];
+	vec2 normals[16];
+	unsigned int size;
+
+	Hull(const vec2 *vertices, unsigned vsize);
+
+};
 
 
 Circle operator*(const mat3 &T, const Circle &C);
 AABB operator*(const mat3 &T, const AABB &B);
 Plane operator*(const mat3 &T, const Plane &P);
 Ray operator*(const mat3 &T, const Ray &R);
+
 Hull operator*(const mat3 &T, const Hull &H);
+bool operator==(const Hull &A, const Hull &B);
