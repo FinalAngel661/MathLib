383242660d926da2c2e9e05763fb6c7c030a4e80
kelsey.davis <kelsey.davis@student.aie.edu.au>
2016-11-15 11:08:53 -0800

    asteriods implemented

---
+++ Components/ObjectCollision.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Asteriod.h"
+#include "PlayerShip.h"
+
+
+void PlayerAsteroidCollision(PlayerShip &player, Asteroid &as);
+
+void AsteroidCollision(Asteroid &as1, Asteroid &as2);
